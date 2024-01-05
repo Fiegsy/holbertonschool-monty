3478d214891f52355f6c2a83c0a793fb2f324dae
leila7575 <leila.louajri@gmail.com>
2024-01-05 11:30:49 -0800

    adding nop prototype to header file

--- monty.h
+++ monty.h
@@ -65,5 +65,6 @@ void pall(stack_t **head, unsigned int count);
 void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
 #endif
