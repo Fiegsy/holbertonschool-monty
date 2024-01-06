236b98d5b05b8a7ce7362a66d60c6d90766c1d03
leila7575 <leila.louajri@gmail.com>
2024-01-06 07:22:28 -0800

    fixed typo mistake in pop function error message

--- pop.c
+++ pop.c
@@ -9,7 +9,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	if(!stack || !*stack)
 	{
-		fprintf(stderr, "L%d: : can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	
