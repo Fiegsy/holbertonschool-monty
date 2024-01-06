5f6f63a1d9b99ffee4ab9a210031afd30d8e31a1
leila7575 <leila.louajri@gmail.com>
2024-01-05 23:06:45 -0800

    adding pint implementation file

--- pint.c
+++ pint.c
@@ -7,7 +7,7 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	if(!stack || !*stack)
+	if(stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: : can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
