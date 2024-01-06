8b981d6bde22bd2fa66cd10f0093b279e9a2549c
leila7575 <leila.louajri@gmail.com>
2024-01-06 07:26:00 -0800

    fixed typo mistake in add.c function error message

--- add.c
+++ add.c
@@ -9,7 +9,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if(!stack || !*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: : can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	
